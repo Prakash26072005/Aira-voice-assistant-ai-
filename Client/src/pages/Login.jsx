f5cc264b01a99841ba614f85e4b61e47f7f20f9a
Prakash26072005 <pm2466599@gmail.com>
2026-08-16 20:39:30 +0530

    Update Login.jsx

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -78,7 +78,8 @@ function Login  ({setUser}) {
             <div className='max-w-7xl mx-auto px-6 py-6 lg:py-24'>
 
                 {/* <div className='grid lg:grid-cols-2 gap-16 items-center'> */}
-                <div className="grid grid-cols-2 gap-16 items-center">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
+                  
                     {/* left */}
                     <div>
                         <div className='inline-flex items-center gap-2 px-4 py-2 rounded-full border border-purple-200 bg-purple-100 text-purple-600 text-sm font-medium'>
